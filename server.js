a5fc043fad481797d7f1576a79fc8cc494363972
Usman Javed <uj123243@gmail.com>
2023-10-30 14:46:46 +0500

    Eighth

--- server.js
+++ server.js
@@ -37,47 +37,47 @@ const server = app.listen(port, (port) => {
 
 
 
-const io = require("socket.io")(server, {
-    cors: {
-        origin: "https://chatwav.vercel.app/", 
-        methods: ["GET", "POST"]
-    }
-});
+// const io = require("socket.io")(server, {
+//     cors: {
+//         origin: "https://chatwav.vercel.app/", 
+//         methods: ["GET", "POST"]
+//     }
+// });
 
 
 
-io.on("connection", (socket) => {
-    console.log("Connected to socket.io");
-    socket.on("setup", (userData) => {
-        console.log(userData._id)
-        socket.join(userData._id);
-        socket.emit("connected");
-    });
+// io.on("connection", (socket) => {
+//     console.log("Connected to socket.io");
+//     socket.on("setup", (userData) => {
+//         console.log(userData._id)
+//         socket.join(userData._id);
+//         socket.emit("connected");
+//     });
 
-    socket.on("join chat", (room) => {
-        socket.join(room);
-        console.log("User Joined Room: " + room);
-    });
-    socket.on("typing", (room) => socket.in(room).emit("typing"));
-    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+//     socket.on("join chat", (room) => {
+//         socket.join(room);
+//         console.log("User Joined Room: " + room);
+//     });
+//     socket.on("typing", (room) => socket.in(room).emit("typing"));
+//     socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-    socket.on("new message", (newMessageRecieved) => {
-        var chat = newMessageRecieved.chat;
+//     socket.on("new message", (newMessageRecieved) => {
+//         var chat = newMessageRecieved.chat;
 
-        if (!chat.user) return console.log("chat.users not defined");
+//         if (!chat.user) return console.log("chat.users not defined");
 
-        chat.user.forEach((user) => {
-            if (user._id == newMessageRecieved.sender._id) return;
+//         chat.user.forEach((user) => {
+//             if (user._id == newMessageRecieved.sender._id) return;
 
-            socket.in(user._id).emit("message recieved", newMessageRecieved);
-        });
-    });
-    socket.on("typing", (room) => socket.in(room).emit("typing"))
-    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"))
-    socket.off("setup", () => {
-        console.log("USER DISCONNECTED");
-        socket.leave(userData._id);
-    });
-});
+//             socket.in(user._id).emit("message recieved", newMessageRecieved);
+//         });
+//     });
+//     socket.on("typing", (room) => socket.in(room).emit("typing"))
+//     socket.on("stop typing", (room) => socket.in(room).emit("stop typing"))
+//     socket.off("setup", () => {
+//         console.log("USER DISCONNECTED");
+//         socket.leave(userData._id);
+//     });
+// });
 
 ConnectToDataBase();
