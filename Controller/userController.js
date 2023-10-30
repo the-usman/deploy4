c4ad02b30ecb99794d21630468a7a370fab95fa9
Usman Javed <uj123243@gmail.com>
2023-10-30 14:36:27 +0500

    Sixth

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,81 +1,80 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../Models/User');
 const jwt = require('jsonwebtoken');
-const config = require('../config.json');
 const bcrypt = require('bcrypt');
 
-const SecretKey = process.env.SECRECT_key;
+const SecretKey = process.env.SECRET_KEY;
 
 const SignUpUser = asyncHandler(async (req, res) => {
-    let success = false;
-    const { name, password, email, pic } = req.body;
+    try {
+        const { name, password, email, pic } = req.body;
 
-    if (!req.body.password || !req.body.name || !req.body.email) {
-        return res.status(400).json({ success, error: "Invalid Request" });
-    }
+        if (!password || !name || !email) {
+            return res.status(400).json({ success: false, error: "Invalid Request" });
+        }
 
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(password.toString(), salt);
 
-    // Use await when generating the salt and hashing the password
-    const salt = await bcrypt.genSalt(10);
-    const secPass = await bcrypt.hash(password.toString(), salt);
+        const existingUser = await User.findOne({ email: email });
+        if (existingUser) {
+            return res.status(400).json({ success: false, error: "User already exists" });
+        }
 
-
-    const ExistUser = await User.findOne({ email: email });
-    if (ExistUser) {
-        return res.status(400).json({ success, error: "User already exists" });
-    }
-
-    const user = await User.create({
-        email: email,
-        name: name,
-        password: secPass,
-        pic: pic
-    });
-
-    if (user) {
-        const data = {
-            user: {
-                id: user.id
-            }
-        };
-        const token = jwt.sign(data, SecretKey, {
-            expiresIn: "30d"
+        const user = await User.create({
+            email: email,
+            name: name,
+            password: secPass,
+            pic: pic
         });
 
-        return res.status(200).json({
-            success: true,
+        if (user) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            };
+            const token = jwt.sign(data, SecretKey, {
+                expiresIn: "30d"
+            });
+
+            return res.status(200).json({
+                success: true,
                 id: user._id,
                 email: user.email,
                 pic: user.pic,
                 name: user.name,
                 token
-            
-        });
-    } else {
-        return res.status(400).json({ success, error: "Failed to create user" });
+            });
+        } else {
+            return res.status(400).json({ success: false, error: "Failed to create user" });
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, error: "Server error" });
     }
 });
 
 const LoginUser = asyncHandler(async (req, res) => {
-    let success = false;
-    const { password, email } = req.body;
+    try {
+        const { password, email } = req.body;
 
-    if (!req.body.password || !req.body.email) {
-        return res.status(400).json({ success, error: "Invalid Request" });
-    }
+        if (!password || !email) {
+            return res.status(400).json({ success: false, error: "Invalid Request" });
+        }
 
+        const existingUser = await User.findOne({ email: email });
+        if (!existingUser) {
+            return res.status(400).json({ success: false, error: "User not found" });
+        }
 
-    const ExistUser = await User.findOne({ email: email });
-    if (!ExistUser) {
-        return res.status(400).json({ success, error: "User not exists" });
-    }
-    const pass = await bcrypt.compare(password, ExistUser.password)
-    if (!pass)
-        return res.status(400).json({ success, error: "Invalid credential" })
-    if (ExistUser) {
+        const pass = await bcrypt.compare(password, existingUser.password);
+        if (!pass) {
+            return res.status(400).json({ success: false, error: "Invalid credentials" });
+        }
+
         const data = {
             user: {
-                id: ExistUser.id
+                id: existingUser.id
             }
         };
         const token = jwt.sign(data, SecretKey, {
@@ -84,53 +83,43 @@ const LoginUser = asyncHandler(async (req, res) => {
 
         return res.status(200).json({
             success: true,
-            id: ExistUser._id,
-            email: ExistUser.email,
-            pic: ExistUser.pic,
-            name: ExistUser.name,
+            id: existingUser._id,
+            email: existingUser.email,
+            pic: existingUser.pic,
+            name: existingUser.name,
             token
         });
-    } else {
-        return res.status(400).json({ success, error: "Failed to login user" });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: "Server error" });
     }
 });
 
-
-
-// For Storing image in the multer
-
-const UpdatedUser = asyncHandler(async () => {
-    const { name, email, password } = req.body;
-    if (!req.body.password || !req.body.name || !req.body.email) {
-        return res.status(400).json({ success, error: "Invalid Request" });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const secPass = await bcrypt.hash(password.toString(), salt);
-    const ExistUser = await User.findOne({ email: email });
-    if (!ExistUser) {
-        return res.status(400).json({ success, error: "User already exists" });
-    }
-
-})
+const UpdatedUser = asyncHandler(async (req, res) => {
+    // Logic for updating user data (if required)
+    // Add necessary implementation or remove if not needed
+});
 
 const allUser = asyncHandler(async (req, res) => {
-    const keyword = req.query.search ? {
-        $or: [
-            { name: { $regex: req.query.search, $options: "i" } },
-            { email: { $regex: req.query.search, $options: "i" } },
-        ]
-    } : {};
-    const user = await User.find({
-        ...keyword,
-        _id: { $ne: req.user.id }
-    });
-    res.send(user)
-})
+    try {
+        const keyword = req.query.search ? {
+            $or: [
+                { name: { $regex: req.query.search, $options: "i" } },
+                { email: { $regex: req.query.search, $options: "i" } },
+            ]
+        } : {};
+        const users = await User.find({
+            ...keyword,
+            _id: { $ne: req.user.id }
+        });
+        res.send(users);
+    } catch (error) {
+        return res.status(500).json({ success: false, error: "Server error" });
+    }
+});
 
 module.exports = {
     SignUpUser,
     LoginUser,
-    allUser,
-    UpdatedUser
+    UpdatedUser,
+    allUser
 };
--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,10 +1,10 @@
-const { SignUpUser, LoginUser, allUser } = require('../Controller/userController')
-const fetchUser = require('../Middleware/fetchUser')
-const router = require('express').Router()
+const express = require('express');
+const { SignUpUser, LoginUser, allUser } = require('../Controller/userController');
+const fetchUser = require('../Middleware/fetchUser');
+const router = express.Router();
 
+router.get('/', fetchUser, allUser);
+router.post('/signup', SignUpUser);
+router.post('/login', LoginUser);
 
-router.get('/', fetchUser , allUser)
-router.post('/signup', SignUpUser)
-router.post('/login', LoginUser)
-
-module.exports = router
+module.exports = router;
